a65f229ddcecbe90317a1c00b8bc579402c6a7b0
Anand raja <meetanandns@gmail.com>
2023-01-24 11:03:26 +0000

    test case

--- src/app/mat-table-infinite/mat-table-infinite.component.spec.ts
+++ src/app/mat-table-infinite/mat-table-infinite.component.spec.ts
@@ -1,23 +1,75 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChangeDetectorRef } from '@angular/core';
+
+import {
+  CdkVirtualScrollViewport,
+  ScrollingModule,
+} from '@angular/cdk/scrolling';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { Subject } from 'rxjs';
 
 import { MatTableInfiniteComponent } from './mat-table-infinite.component';
 
-describe('MatTableInfiniteComponent', () => {
+class MockChangeDetectorRefClass extends ChangeDetectorRef {
+  markForCheck(): void {}
+  detach(): void {}
+  detectChanges(): void {}
+  checkNoChanges(): void {}
+  reattach(): void {}
+}
+
+class MockCdkVirtualScrollViewport extends CdkVirtualScrollViewport {
+  // @Input() itemSize = '140';
+  override scrolledIndexChange: Subject<number> = new Subject<number>();
+  // elementScrolled: Subject<Event> = new Subject<Event>();
+  override setRenderedRange(): void {}
+  override getRenderedRange(): any {}
+
+  // static itemSize = 130;
+  // add any other properties and methods that are needed for your test
+}
+
+fdescribe('MatTableInfiniteComponent', () => {
   let component: MatTableInfiniteComponent;
   let fixture: ComponentFixture<MatTableInfiniteComponent>;
+  let changeDetectorRef: ChangeDetectorRef;
+  let virtualScrollViewport: CdkVirtualScrollViewport;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatTableInfiniteComponent ]
-    })
-    .compileComponents();
+      declarations: [MatTableInfiniteComponent],
+      imports: [HttpClientTestingModule, ScrollingModule, MatToolbarModule],
+      providers: [
+        {
+          provide: ChangeDetectorRef,
+          useClass: MockChangeDetectorRefClass,
+        },
+        {
+          provide: CdkVirtualScrollViewport,
+          useClass: MockCdkVirtualScrollViewport,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MatTableInfiniteComponent);
+
+    changeDetectorRef = TestBed.inject(ChangeDetectorRef);
+    virtualScrollViewport = TestBed.inject(CdkVirtualScrollViewport);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check \'ngAfterViewInit\'', () => {
+    component.ngAfterViewInit();
+    changeDetectorRef.detectChanges();
+    spyOn(virtualScrollViewport, 'elementScrolled').and.callThrough();
+    expect(virtualScrollViewport.elementScrolled).toHaveBeenCalled();
   });
 });
--- src/app/mat-table-infinite/mat-table-infinite.component.ts
+++ src/app/mat-table-infinite/mat-table-infinite.component.ts
@@ -1,9 +1,16 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  NgZone,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { filter, map, pairwise, Subscription, throttleTime, timer } from 'rxjs';
 // import { MatTableDataSource } from '@angular/material/table';
-
 
 export interface PeriodicElement {
   name: string;
@@ -13,26 +20,27 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
   selector: 'app-mat-table-infinite',
   templateUrl: './mat-table-infinite.component.html',
   styleUrls: ['./mat-table-infinite.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.Default,
 })
-export class MatTableInfiniteComponent implements OnInit, AfterViewInit, OnDestroy {
-
+export class MatTableInfiniteComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   @ViewChild('scroller')
   scroller!: CdkVirtualScrollViewport;
 
@@ -50,23 +58,25 @@ export class MatTableInfiniteComponent implements OnInit, AfterViewInit, OnDestr
   title = 'Infinite Scroll Mat-Table using "ScrollingModule"';
 
   // dataSource = new MatTableDataSource<PeriodicElement>();
-  
-  dataSource: PeriodicElement[] = [];
-  
 
-  constructor(private ngZone: NgZone) { }
+  dataSource: PeriodicElement[] = [];
+
+  constructor(private ngZone: NgZone) {}
 
   ngAfterViewInit(): void {
-    this.unsub = this.scroller.elementScrolled().pipe(
-      map(() => this.scroller.measureScrollOffset('bottom')),
-      pairwise(),
-      filter(([y1, y2]) => (y2 < y1 && y2 < 140)),
-      throttleTime(200)
-    ).subscribe(() => {
-      this.ngZone.run(() => {
-        (this.maxItems > this.listItems.length) && this.fetchMore();
+    this.unsub = this.scroller
+      .elementScrolled()
+      .pipe(
+        map(() => this.scroller.measureScrollOffset('bottom')),
+        pairwise(),
+        filter(([y1, y2]) => y2 < y1 && y2 < 140),
+        throttleTime(200),
+      )
+      .subscribe(() => {
+        this.ngZone.run(() => {
+          this.maxItems > this.listItems.length && this.fetchMore();
+        });
       });
-    })
   }
 
   ngOnInit(): void {
@@ -74,21 +84,18 @@ export class MatTableInfiniteComponent implements OnInit, AfterViewInit, OnDestr
   }
 
   ngOnDestroy(): void {
-    if(this.unsub) {
+    if (this.unsub) {
       this.unsub.unsubscribe();
     }
 
-    this.timerSub.unsubscribe();
+    this.timerSub && this.timerSub.unsubscribe();
   }
-  
 
-  fetchMore(): void  {
-
+  fetchMore(): void {
     const newItems: any[] = [];
     for (let i = 0; i < ELEMENT_DATA.length; i++) {
       newItems.push(ELEMENT_DATA[i]);
     }
-
 
     this.loading = true;
     this.timerSub = timer(1000).subscribe(() => {
@@ -96,7 +103,5 @@ export class MatTableInfiniteComponent implements OnInit, AfterViewInit, OnDestr
       this.listItems = [...this.listItems, ...newItems];
       this.dataSource = this.listItems;
     });
-    
   }
-
 }
